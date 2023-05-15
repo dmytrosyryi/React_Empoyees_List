403352055b49038f1d26adc190b8a2d7fd227aac
Dmytro Syryi <syryi.dmytro@gmail.com>
2023-05-15 18:05:39 +0200

    first Commit

---
+++ src/components/app/app.js
@@ -0,0 +1,92 @@
+import { Component } from 'react/cjs/react.production.min';
+
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
+import './app.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: [
+        { name: "John S.", salary: 800, increase: false, like: true, id: 1 },
+        { name: "Albert G.", salary: 3000, increase: true, like: false, id: 2 },
+        { name: "Grygoryi T.", salary: 5000, increase: false, like: false, id: 3 }
+      ]
+    }
+    this.maxId = 4
+  }
+
+  deleteItem = (id) => {
+    this.setState(({ data }) => {
+      return {
+        data: data.filter(item => item.id !== id)
+      }
+    })
+  }
+
+  addItem = (name, salary) => {
+    const newItem = {
+      name,
+      salary,
+      increase: false,
+      id: this.maxId++,
+      like: false
+    }
+    if (name.length < 3) {
+      return this.state
+    } else {
+      this.setState(({ data }) => {
+        const newArr = [...data, newItem]
+        return {
+          data: newArr
+        }
+
+      })
+    }
+
+  }
+
+  onToggleProp = (id, prop) => {
+    this.setState(({ data }) => ({
+      data: data.map(item => {
+        if (item.id === id) {
+          return { ...item, [prop]: !item[prop] }
+        }
+        return item;
+      })
+    }))
+  }
+
+
+
+
+  render() {
+    const employees = this.state.data.length
+    const increased = this.state.data.filter(item => item.increase).length
+    return (<div className="app">
+      <AppInfo
+        employees={employees}
+        increased={increased} />
+
+      <div className="search-panel">
+        <SearchPanel />
+        <AppFilter />
+      </div>
+
+      <EmployeesList
+        data={this.state.data}
+        onDelete={this.deleteItem}
+        onToggleProp={this.onToggleProp}
+      />
+      <EmployeesAddForm
+        onAdd={this.addItem} />
+    </div>)
+  }
+}
+
+export default App;
